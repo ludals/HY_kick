bc8b5a60a23389c3c8d74932c6f1ef75c06d03f8
Yoosang Lee <dldbtkd0712@hanyang.ac.kr>
2023-11-18 16:04:23 +0900

    Fix: map func

--- my-app/src/component/upcoming/Upcoming.jsx
+++ my-app/src/component/upcoming/Upcoming.jsx
@@ -11,19 +11,19 @@ const Upcoming = (props) => {
     const awayComponent = document.getElementsByClassName('Away');
 
     useEffect(()=>{
-        Array.from(homeComponent).map((tmp) => {
+        Array.from(homeComponent).forEach((tmp) => {
             tmp.style.fontSize = `${26 - 1.5 * (tmp.textContent.length)}px`;
             tmp.style.fontWeight = "bold";
             tmp.style.marginLeft = tmp.textContent.length > 4 ? "1.5rem" : "1rem";
             tmp.style.marginRight = tmp.textContent.length > 4 ? "1.5rem" : "2rem";
         });
-        Array.from(awayComponent).map((tmp) => {
+        Array.from(awayComponent).forEach((tmp) => {
             tmp.style.fontSize = `${26 - 1.5 *(tmp.textContent.length)}px`;
             tmp.style.fontWeight = "bold";
             tmp.style.marginLeft = tmp.textContent.length > 4 ? "1.5rem" : "2rem";
             tmp.style.marginRight = tmp.textContent.length > 4 ? "1.5rem" : "1rem";
         });
-    }, [])
+    }, [homeComponent, awayComponent])
 
     const openModal = () => {
         setIsModalOpen(true);
