526ccc1a966e6352cf28174068c2a8148d385cef
Yoosang Lee <dldbtkd0712@hanyang.ac.kr>
2023-11-24 02:12:05 +0900

    Feat: Calender date select

--- my-app/src/component/schedule/Schedule.jsx
+++ my-app/src/component/schedule/Schedule.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import PropTypes from 'prop-types';
 import gotoLeft from "../../asset/gotoLeft.png"
@@ -13,135 +13,135 @@ const Schedule = () => {
     const [year, setYear] = useState(now.getFullYear());    //selected year
     const [month, setMonth] = useState(now.getMonth() + 1); //selected month
     const [date, setDate] = useState(now.getDate());        //selected day
+    const daysRef = useRef([]);
+    const [prevDate, setPrevDate] = useState();
     const day = ["일", "월", "화", "수", "목", "금", "토"];
     const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     const [matchInfo, setMatchInfo] = useState(matchData.filter((state) => new Date(state.date).getTime() === new Date(`${year}-${month}-${date}`).getTime()));
-    const [monthData, setMonthData] = useState({
-        date: new Date(year, month - 1, date),        //just move week
-        days: Array.from({ length: days[month - 1] }, (_, index) => index + 1),       //date info of selected month
-    });
-    const [monthDiv, setMonthDiv] = useState(monthData.days && monthData.days.map((value, index) => {
-        return (
-            <DayWrapper key={index + 1}>
-                <div className="day">{day[(new Date(year, month - 1, value)).getDay()]}</div>
-                <div className="date">{value}</div>
-                <div className="selectBar"></div>
-            </DayWrapper>
-        );
-    }));
+    const [weekData, setWeekData] = useState(new Date(year, month - 1, date));                                        //just move week
+    const [monthArray, setMonthArray] = useState(Array.from({ length: days[month - 1] }, (_, index) => index + 1));       //date info of month
+    const [monthDiv, setMonthDiv] = useState();
+    const [weekDiv, setWeekDiv] = useState();
 
-    const [weekDiv, setWeekDiv] = useState(monthDiv.filter(value =>
-        (date - now.getDay()) <= value.key && (date + (6 - now.getDay())) >= value.key
-    ));
-
-    const loadMatchData = useCallback((year, month, date) => {
+    const loadMatchData = (year, month, date) => {
         return matchData.filter((state) => new Date(state.date).getTime() === new Date(`${year}-${month}-${date}`).getTime());
-    }, [matchData]);
+    }
 
     useEffect(() => {
-        const matchInfo = loadMatchData(year, month, date);
-        setMatchInfo(matchInfo);
-    }, [year, month, date, loadMatchData]);
-
-    const selectDate = useCallback((Nyear, Nmonth, Ndate, value) => {
-        if (!loadMatchData(monthData.date.getFullYear(), monthData.date.getMonth() + 1, value).length)
-            return;
-        for (let id of ["day", "date", "bar"]) {
-            if (document.getElementById(year + "" + month + "" + value + id) && document.getElementById(year + "" + month + "" + date + id)) {
-                if (id === "bar") {
-                    document.getElementById(year + "" + month + "" + value + id).style.background = "blue";
-                    document.getElementById(year + "" + month + "" + date + id).style.background = value !== date ? "white" : "blue";
-                    console.log("색바꿈");
-                }
-                else {
-                    document.getElementById(year + "" + month + "" + value + id).style.color = "blue";
-                    document.getElementById(year + "" + month + "" + date + id).style.color = value !== date ? "black" : "blue";
-                }
-            }
-        }
-        setYear(monthData.date.getFullYear());
-        setMonth(monthData.date.getMonth() + 1);
-        setDate(value);
-        console.log(year + "" + month + "" + date);
-        console.log(year + "" + month + "" + value);
-        console.log(monthData.date);
-    }, [year, month, date, monthData.date]);
-
-    useEffect(() => {
-        setMonthDiv(monthData.days && monthData.days.map((value) => {
+        setMonthDiv(monthArray && monthArray.map((value) => {
             return (
-                <DayWrapper key={value} onClick={() => selectDate(year, month, date, value)} $isActive={loadMatchData(monthData.date.getFullYear(), monthData.date.getMonth() + 1, value).length}>
-                    <div id={monthData.date.getFullYear() + "" + (monthData.date.getMonth() + 1) + "" + value + "day"} className="day">{day[(new Date(monthData.date.getFullYear(), monthData.date.getMonth(), value)).getDay()]}</div>
-                    <div id={monthData.date.getFullYear() + "" + (monthData.date.getMonth() + 1) + "" + value + "date"} className="date">{value}</div>
-                    <div id={monthData.date.getFullYear() + "" + (monthData.date.getMonth() + 1) + "" + value + "bar"} className="selectBar"></div>
+                <DayWrapper key={value} id={"daywrapper"} $isActive={loadMatchData(weekData.getFullYear(), weekData.getMonth() + 1, value).length}>
+                    <div id={weekData.getFullYear() + "" + (weekData.getMonth() + 1) + "" + value} className="day">{day[(new Date(weekData.getFullYear(), weekData.getMonth(), value)).getDay()]}</div>
+                    <div id={weekData.getFullYear() + "" + (weekData.getMonth() + 1) + "" + value + "date"} className="date">{value}</div>
+                    <div id={weekData.getFullYear() + "" + (weekData.getMonth() + 1) + "" + value + "bar"} className="selectBar"></div>
                 </DayWrapper>
             );
-        }))// eslint-disable-next-line
-    }, [monthData, selectDate])
+        }))
+    }, [])
 
     useEffect(() => {
-        setWeekDiv(monthDiv.filter(value =>
-            (monthData.date.getDate() - monthData.date.getDay()) <= value.key && (monthData.date.getDate() + (6 - monthData.date.getDay())) >= value.key
+        const weeks = document.querySelectorAll("#daywrapper");
+        for (let week of weeks) {
+            week.addEventListener("click", () => selectDate(weekData.getFullYear(), weekData.getMonth() + 1, Number(week.children[1].outerText)));
+        }
+    }, [weekDiv, year, month, date])
+
+    useEffect(() => {
+        monthDiv && setWeekDiv(monthDiv.filter(value =>
+            (weekData.getDate() - weekData.getDay()) <= value.key && (weekData.getDate() + (6 - weekData.getDay())) >= value.key
         ));
-    }, [monthData.date, monthDiv])
+        selectDate(year, month, date);
+    }, [monthDiv])
 
+    function selectDate(Nyear, Nmonth, Nvalue) {
+        const matchInfo = loadMatchData(Nyear, Nmonth, Nvalue);
+        if (matchInfo.length === 0)
+            return;
 
+        for (let id of ["day", "date", "bar"]) {
+            const curId = Nyear + "" + Nmonth + "" + Nvalue + id;
+            const prevId = year + "" + month + "" + date + id;
+            console.log(curId);
+            if (document.getElementById(curId)) {
+                console.log(document.getElementById(curId));
+                if (id === "bar") {
+                    document.getElementById(curId).style.background = "blue";
+                    console.log(curId, "색바꿈");
+                    if (document.getElementById(prevId) && curId !== prevId) {
+                        document.getElementById(prevId).style.background = "white";
+                    }
+                }
+                else {
+                    document.getElementById(curId).style.color = "blue";
+                    if (document.getElementById(prevId) && curId !== prevId) {
+                        document.getElementById(prevId).style.color = "black";
+                    }
+                }
 
+            }
+        }
+        setYear(Nyear);
+        setMonth(Nmonth);
+        setDate(Nvalue);
+        setPrevDate(new Date(year, month - 1, date));
+        setMatchInfo(matchInfo);
+    }
+
+    useEffect(() => {
+        setMonthDiv(monthArray && monthArray.map((value) => {
+            return (
+                <DayWrapper key={value} id={"daywrapper"} $isActive={loadMatchData(weekData.getFullYear(), weekData.getMonth() + 1, value).length}>
+                    <div id={weekData.getFullYear() + "" + (weekData.getMonth() + 1) + "" + value + "day"} className="day">{day[(new Date(weekData.getFullYear(), weekData.getMonth(), value)).getDay()]}</div>
+                    <div id={weekData.getFullYear() + "" + (weekData.getMonth() + 1) + "" + value + "date"} className="date">{value}</div>
+                    <div id={weekData.getFullYear() + "" + (weekData.getMonth() + 1) + "" + value + "bar"} className="selectBar"></div>
+                </DayWrapper>
+            );
+        }))
+        monthDiv && setWeekDiv(monthDiv.filter(value =>
+            (weekData.getDate() - weekData.getDay()) <= value.key && (weekData.getDate() + (6 - weekData.getDay())) >= value.key
+        ));
+    }, [weekData])
 
     const prevMonth = () => {
-        if (monthData.date.getMonth() === 0) {
-            setMonthData((prev) => ({
-                days: Array.from({ length: days[11] }, (_, index) => index + 1),
-                date: new Date(prev.date.getFullYear() - 1, 11, days[11]),
-            }));
+        if (weekData.getMonth() === 0) {
+            setMonthArray(Array.from({ length: days[11] }, (_, index) => index + 1));
+            setWeekData((prev) => new Date(prev.getFullYear() - 1, 11, days[11]));
         }
         else {
-            setMonthData((prev) => ({
-                days: Array.from({ length: days[prev.date.getMonth() - 1] }, (_, index) => index + 1),
-                date: new Date(prev.date.getFullYear(), prev.date.getMonth() - 1, days[prev.date.getMonth() - 1]),
-            }));
+            setMonthArray(Array.from({ length: days[weekData.getMonth() - 1] }, (_, index) => index + 1));
+            setWeekData((prev) => new Date(prev.getFullYear(), prev.getMonth() - 1, days[prev.getMonth() - 1]))
         }
     };
 
     const nextMonth = () => {
-        if (monthData.date.getMonth() === 11) {
-            setMonthData((prev) => ({
-                days: Array.from({ length: days[0] }, (_, index) => index + 1),
-                date: new Date(prev.date.getFullYear() + 1, 0, 1),
-            }));
+        if (weekData.getMonth() === 11) {
+            setMonthArray(Array.from({ length: days[0] }, (_, index) => index + 1));
+            setWeekData((prev) => new Date(prev.getFullYear() + 1, 0, 1));
         }
         else {
-            setMonthData((prev) => ({
-                days: Array.from({ length: days[prev.date.getMonth() + 1] }, (_, index) => index + 1),
-                date: new Date(prev.date.getFullYear(), prev.date.getMonth() + 1, 1),
-            }));
+            setMonthArray(Array.from({ length: days[weekData.getMonth() + 1] }, (_, index) => index + 1));
+            setWeekData((prev) => new Date(prev.getFullYear(), prev.getMonth() + 1, 1));
         }
     };
 
     const prevWeek = () => {
-        setMonthData((prev) => ({
-            ...prev,
-            date: new Date(prev.date.getFullYear(), prev.date.getMonth(), prev.date.getDate() - (prev.date.getDay() + 1)),
-        }));
+        setWeekData((prev) => (new Date(prev.getFullYear(), prev.getMonth(), prev.getDate() - (prev.getDay() + 1))))
     }
 
     const nextWeek = () => {
-        setMonthData((prev) => ({
-            ...prev,
-            date: new Date(prev.date.getFullYear(), prev.date.getMonth(), prev.date.getDate() + (-prev.date.getDay() + 7)),
-        }));
+        setWeekData((prev) => new Date(prev.getFullYear(), prev.getMonth(), prev.getDate() + (-prev.getDay() + 7)));
     }
 
     return (
         <CalenderWrapper>
             <MonthSelectWrapper>
                 <img src={arrowLeft} alt="left" style={{ width: "25px", height: "25px", cursor: "pointer" }} onClick={prevMonth}></img>
-                <div style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{monthData.date.getFullYear()}.{monthData.date.getMonth() < 9 ? 0 : null}{monthData.date.getMonth() + 1}</div>
+                <div style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{weekData.getFullYear()}.{weekData.getMonth() < 9 ? 0 : null}{weekData.getMonth() + 1}</div>
                 <img src={arrowRight} alt="right" style={{ width: "25px", height: "25px", cursor: "pointer" }} onClick={nextMonth}></img>
             </MonthSelectWrapper>
-            <WeekWrapper $date={monthData.date} $days={days}>
+            <WeekWrapper $date={weekData} $days={days}>
                 <img src={gotoLeft} alt="gotoLeft" className="prevWeek" onClick={prevWeek} />
-                <WeekView $date={monthData.date}>
+                <WeekView $date={weekData}>
                     {weekDiv}
                 </WeekView>
                 <img src={gotoRight} alt="gotoRight" className="nextWeek" onClick={nextWeek} />
