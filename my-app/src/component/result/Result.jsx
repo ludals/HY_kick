78cbdac4e758933f0efd8ff9fb298f4cdaf41ba1
LeeYooSang <dldbtkd0712@naver.com>
2024-01-11 11:47:01 +0900

    feat: calender & result interaction

--- my-app/src/component/result/Result.jsx
+++ my-app/src/component/result/Result.jsx
@@ -1,5 +1,12 @@
-const Result = () => {
+import { useParams } from "react-router-dom";
 
+const Result = () => {
+  const matchID = useParams();
+  return (
+    <>
+      <div>{matchID.id}</div>
+    </>
+  );
 }
 
 export default Result;
