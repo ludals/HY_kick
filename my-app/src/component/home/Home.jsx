483c3f8346d82b4cd774af4e1bff6a050368519c
LeeYooSang <dldbtkd0712@naver.com>
2024-01-31 22:27:47 +0900

    Fix: back to calender

--- my-app/src/component/home/Home.jsx
+++ my-app/src/component/home/Home.jsx
@@ -4,14 +4,16 @@ import Schedule from "../schedule/Schedule";
 import { useSelector } from "react-redux";
 
 const Home = () => {
-    const matchData = useSelector((state) => state.match.value);
-    return(
-        <>
-            <Upcoming matches={matchData.match}></Upcoming>
-            <Ranking></Ranking>
-            <Schedule></Schedule>
-        </>
-    );
+  const matchData = useSelector((state) => state.match.value);
+  sessionStorage.removeItem('curDate');
+
+  return (
+    <>
+      <Upcoming matches={matchData.match}></Upcoming>
+      <Ranking></Ranking>
+      <Schedule></Schedule>
+    </>
+  );
 };
 
 export default Home;
